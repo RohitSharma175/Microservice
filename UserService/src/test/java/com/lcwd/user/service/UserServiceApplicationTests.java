d0c7a11d3022d311acd1222bbcd2322c8f7ef8b0
Rohit Sharma <rohitk@dreamsol.biz>
2023-01-01 01:15:03 +0530

    Api gateway added and config server

--- UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
@@ -1,7 +1,16 @@
 package com.lcwd.user.service;
 
+import com.lcwd.user.service.entities.Rating;
+import com.lcwd.user.service.external.services.RatingService;
+import com.netflix.discovery.converters.Auto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLOutput;
+
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -9,5 +18,14 @@ class UserServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Autowired
+	private RatingService ratingService;
+//	@Test
+//	void createRating(){
+//		Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("This is created by the fein client").build();
+//		Rating saveRating = ratingService.createRating(rating);
+//		System.out.println(saveRating);
+//	}
 
 }
